0e57975b3d356e79637d19cee9a9b3f5e80d6f26
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-22 22:08:12 +0300

    Add files via upload

---
+++ FunctionsAsParametersSince8.java
@@ -0,0 +1,23 @@
+package javatraining.oletsky.lambdasandstreams;
+
+/**
+ * @author O.Oletsky
+ * Demonstrating functions as psrameners on the base of functional interfaces
+ */
+
+import java.util.function.Function;
+
+public class FunctionsAsParametersSince8 {
+
+    public static void main(String[] args) {
+        showTable(Math::sin);
+
+    }
+
+    static void showTable(Function<Double,Double> fun) {
+        for (double d=0.; d<=1.; d+=0.1) {
+            System.out.printf("%8.4f: %8.4f\n",d,fun.apply(d));
+        }
+    }
+
+}
